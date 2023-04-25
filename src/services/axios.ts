00109c8cf1844406d5c0e07008ed4a03ad5deab2
CAHNCHOYS <lullullullul94@gamil.com>
2023-04-25 12:16:38 +0300

    env

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://crm-backend-mocha.vercel.app/api",
   headers: {
     "Content-Type": "application/json"
   }
